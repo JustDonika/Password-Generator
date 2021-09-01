dd22332eed442a0a508c717497e41430839e2d0b
JustDonika <86822565+JustDonika@users.noreply.github.com>
2021-09-02 03:02:40 +1200

    Add files via upload

---
+++ TextOrigin.java
@@ -0,0 +1,8 @@
+import java.io.*;
+
+public class TextOrigin {
+    public static void main(String[] args) throws IOException {
+        PasswordManager PM = new PasswordManager();
+        PM.main(args);
+    }
+}
